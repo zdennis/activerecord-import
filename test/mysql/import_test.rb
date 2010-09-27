5fa9b930034b28efd301a0a81463d64b101faf0d
Zach Dennis <zach.dennis@gmail.com>
2010-09-26 22:10:12 -0400

    Updating MySQL import tests to load files in a ruby1.9 friendly way.

--- test/mysql/import_test.rb
+++ test/mysql/import_test.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
 
-require "test/support/mysql/assertions"
-require "test/support/mysql/import_examples"
+require File.expand_path(File.dirname(__FILE__) + '/../support/mysql/assertions')
+require File.expand_path(File.dirname(__FILE__) + '/../support/mysql/import_examples')
 
 should_support_mysql_import_functionality
