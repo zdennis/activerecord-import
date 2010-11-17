1798a04dd7aa9a6e918c075d2b1b5b3764b6b5a6
Ben Woosley <ben.woosley@gmail.com>
2010-11-17 00:20:00 -0800

    Enable ImportSupport on Postgresql - the test suite now passes in full

--- lib/activerecord-import/adapters/postgresql_adapter.rb
+++ lib/activerecord-import/adapters/postgresql_adapter.rb
@@ -1,5 +1,11 @@
 module ActiveRecord::Import::PostgreSQLAdapter
   module InstanceMethods
+    def self.included(klass)
+      klass.instance_eval do
+        include ActiveRecord::Import::ImportSupport
+      end
+    end
+
     def next_value_for_sequence(sequence_name)
       %{nextval('#{sequence_name}')}
     end
