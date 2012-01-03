b5261d0225b5b4da2a7839b1a48a5872313b9f7d
Yehuda Katz <wycats@gmail.com>
2012-01-03 00:41:04 -0800

    Clean up module usage. There's no need to use InstanceMethods -- normal Ruby modules and includes will work fine here.

--- lib/activerecord-import/active_record/adapters/mysql2_adapter.rb
+++ lib/activerecord-import/active_record/adapters/mysql2_adapter.rb
@@ -2,5 +2,5 @@ require "active_record/connection_adapters/mysql2_adapter"
 require "activerecord-import/adapters/mysql_adapter"
 
 class ActiveRecord::ConnectionAdapters::Mysql2Adapter
-  include ActiveRecord::Import::MysqlAdapter::InstanceMethods
+  include ActiveRecord::Import::MysqlAdapter
 end
