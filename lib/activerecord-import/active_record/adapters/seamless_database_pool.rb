cc0185abb31bae72a91dbe2f9f5219237664e963
Evan Petrie <epetrie@shoedazzle.com>
2012-07-24 17:02:12 -0700

    make work with seamless database pool

---
+++ lib/activerecord-import/active_record/adapters/seamless_database_pool.rb
@@ -0,0 +1,6 @@
+require "active_record/connection_adapters/seamless_database_pool_adapter"
+require "activerecord-import/adapters/mysql_adapter"
+
+class ActiveRecord::ConnectionAdapters::SeamlessDatabasePoolAdapter
+  include ActiveRecord::Import::MysqlAdapter
+end
