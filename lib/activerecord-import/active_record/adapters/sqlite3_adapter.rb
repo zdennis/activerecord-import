43bd628e0578974b7c22d712932554243daaf258
mildmojo <theaterfire@yahoo.com>
2012-10-01 02:42:33 -0400

    Fixes case-sensitive SQLite3 adapter name typos.
    The SQLite3 adapter name was referenced as
    `ActiveRecord::ConnectionAdapters::Sqlite3Adapter`, but the
    ActiveRecord class is spelled `SQLite3Adapter` (upper-case "SQL"). As
    a result, `active_record/adapters/sqlite3_adapter.rb` wasn't actually
    including the methods from `adapters/sqlite3_adapter.rb` into the AR
    adapter class.
    
    This adds a test for proper inheritance and fixes the spelling of
    `SQLite3Adapter`, unifying all references to the ActiveRecord version.

--- lib/activerecord-import/active_record/adapters/sqlite3_adapter.rb
+++ lib/activerecord-import/active_record/adapters/sqlite3_adapter.rb
@@ -1,7 +1,7 @@
 require "active_record/connection_adapters/sqlite3_adapter"
 require "activerecord-import/adapters/sqlite3_adapter"
 
-class ActiveRecord::ConnectionAdapters::Sqlite3Adapter
-  include ActiveRecord::Import::Sqlite3Adapter
+class ActiveRecord::ConnectionAdapters::SQLite3Adapter
+  include ActiveRecord::Import::SQLite3Adapter
 end
 
