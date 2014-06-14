f396c0f67bcfa736e5733cdffb7e5994ded1070d
Matthieu Paret <mathieu@ifeelgoods.com>
2014-06-14 17:36:01 +0200

    Add travis (partially, jruby and sqlite3 not runnning so far)

---
+++ test/travis/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+set +x
+
+bundle exec rake test:em_mysql2               # Run tests for em_mysql2
+bundle exec rake test:mysql                   # Run tests for mysql
+bundle exec rake test:mysql2                  # Run tests for mysql2
+bundle exec rake test:mysql2spatial           # Run tests for mysql2spatial
+bundle exec rake test:mysqlspatial            # Run tests for mysqlspatial
+bundle exec rake test:postgis                 # Run tests for postgis
+bundle exec rake test:postgresql              # Run tests for postgresql
+bundle exec rake test:seamless_database_pool  # Run tests for seamless_database_pool
+bundle exec rake test:spatialite              # Run tests for spatialite
+# so far the version installed in travis seems < 3.7.11 so we cannot test sqlite3 on it
+# bundle exec rake test:sqlite3
+
+#jruby
+#bundle exec rake test:jdbcmysql               # Run tests for jdbcmysql
+#bundle exec rake test:jdbcpostgresql          # Run tests for jdbcpostgresql
