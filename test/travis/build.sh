81f549127f66f86f1b3b55c5c129a4a11a04abca
Zach Dennis <zach.dennis@gmail.com>
2014-12-19 10:48:44 -0500

    Also, run the test suite for ActiveRecord 4.1.x.

--- test/travis/build.sh
+++ test/travis/build.sh
@@ -17,3 +17,15 @@ bundle exec rake test:spatialite              # Run tests for spatialite
 #jruby
 #bundle exec rake test:jdbcmysql               # Run tests for jdbcmysql
 #bundle exec rake test:jdbcpostgresql          # Run tests for jdbcpostgresql
+
+export AR_VERSION=4.1
+bundle update activerecord
+bundle exec rake test:em_mysql2               # Run tests for em_mysql2
+bundle exec rake test:mysql                   # Run tests for mysql
+bundle exec rake test:mysql2                  # Run tests for mysql2
+bundle exec rake test:mysql2spatial           # Run tests for mysql2spatial
+bundle exec rake test:mysqlspatial            # Run tests for mysqlspatial
+bundle exec rake test:postgis                 # Run tests for postgis
+bundle exec rake test:postgresql              # Run tests for postgresql
+bundle exec rake test:seamless_database_pool  # Run tests for seamless_database_pool
+bundle exec rake test:spatialite              # Run tests for spatialite
