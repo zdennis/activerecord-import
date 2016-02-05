8ed116ba05667472ac65d9fb8eb1e35eae81850b
Erik Michaels-Ober <erik@instacart.com>
2016-02-04 17:49:55 -0800

    Recursively import all has_one/has_many associations, not just autosave

--- test/models/book.rb
+++ test/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ActiveRecord::Base
-  belongs_to :topic, :inverse_of=>:books
-  has_many :chapters, :autosave => true, :inverse_of => :book
-  has_many :end_notes, :autosave => true, :inverse_of => :book
+  belongs_to :topic, :inverse_of => :books
+  has_many :chapters, :inverse_of => :book
+  has_many :end_notes, :inverse_of => :book
   if ENV['AR_VERSION'].to_f >= 4.1
     enum status: [:draft, :published]
   end
