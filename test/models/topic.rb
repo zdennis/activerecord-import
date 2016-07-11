2feceefa91950b8114641c4957c8707a73f0aac8
Jordan Owens <jkowens@gmail.com>
2016-07-11 15:29:40 -0400

    Add test for bypassing uniqueness validators

--- test/models/topic.rb
+++ test/models/topic.rb
@@ -1,6 +1,7 @@
 class Topic < ActiveRecord::Base
   validates_presence_of :author_name
   validates :title, numericality: { only_integer: true }, on: :context_test
+  validates :title, uniqueness: true
 
   has_many :books, inverse_of: :topic
   belongs_to :parent, class_name: "Topic"
