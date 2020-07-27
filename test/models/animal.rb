af01ab35fc7b454e08745fabac0b9ac34ec249e0
AhMohsen46 <ahmad.mohsen46@gmail.com>
2020-07-28 01:26:04 +0200

    add test cases for after_initialize

--- test/models/animal.rb
+++ test/models/animal.rb
@@ -1,5 +1,5 @@
 class Animal < ActiveRecord::Base
-  after_initialize :validate_name_presence, :if => :new_record?
+  after_initialize :validate_name_presence, if: :new_record?
   def validate_name_presence
     raise ArgumentError if name.nil?
   end
