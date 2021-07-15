10ad2a723d43ad483e46ba5b4c1162acf909cdd9
Jordan Owens <jkowens@gmail.com>
2021-07-15 17:42:37 -0400

    Include ActiveRecordImport in mysql2 connection adapter

--- lib/activerecord-import/active_record/adapters/jdbcmysql_adapter.rb
+++ lib/activerecord-import/active_record/adapters/jdbcmysql_adapter.rb
@@ -1,6 +1,6 @@
-require "active_record/connection_adapters/mysql_adapter"
-require "activerecord-import/adapters/mysql_adapter"
+require "active_record/connection_adapters/mysql2_adapter"
+require "activerecord-import/adapters/mysql2_adapter"
 
-class ActiveRecord::ConnectionAdapters::MysqlAdapter
-  include ActiveRecord::Import::MysqlAdapter
+class ActiveRecord::ConnectionAdapters::Mysql2Adapter
+  include ActiveRecord::Import::Mysql2Adapter
 end
