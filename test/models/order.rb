18a23e0f6215e191b4b8a0919a0de49b1b53222d
Uladzimiro <dvaraf@gmail.com>
2021-09-21 23:31:30 +0200

    indentation

--- test/models/order.rb
+++ test/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ActiveRecord::Base
   belongs_to :customer,
-             inverse_of: :orders,
-             primary_key: %i[account_id id],
-             foreign_key: %i[account_id customer_id]
+    inverse_of: :orders,
+    primary_key: %i[account_id id],
+    foreign_key: %i[account_id customer_id]
 end
