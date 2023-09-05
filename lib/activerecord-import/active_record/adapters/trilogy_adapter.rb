02f097475ec4b0aaecd94fe81d69443d9fbe5d54
Zack Mariscal <zmariscal@gmail.com>
2023-09-05 08:15:10 -0700

    follow the convention of calling the adapter from the connection adapter

--- lib/activerecord-import/active_record/adapters/trilogy_adapter.rb
+++ lib/activerecord-import/active_record/adapters/trilogy_adapter.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require "active_record/connection_adapters/trilogy_adapter"
-require "activerecord-import/adapters/mysql2_adapter"
+require "activerecord-import/adapters/trilogy_adapter"
 
 class ActiveRecord::ConnectionAdapters::TrilogyAdapter
-  include ActiveRecord::Import::Mysql2Adapter
+  include ActiveRecord::Import::TrilogyAdapter
 end
