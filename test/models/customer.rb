3e12169f741f13f58073a076e7bbedc9b01637c9
Jordan Owens <jkowens@gmail.com>
2023-11-17 23:27:28 -0500

    Fix failing specs

--- test/models/customer.rb
+++ test/models/customer.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class Customer < ActiveRecord::Base
-  has_many :orders,
-    inverse_of: :customer,
-    primary_key: %i(account_id id),
-    foreign_key: %i(account_id customer_id)
+  unless ENV["SKIP_COMPOSITE_PK"]
+    has_many :orders,
+      inverse_of: :customer,
+      primary_key: %i(account_id id),
+      foreign_key: %i(account_id customer_id)
+  end
 end
--- test/models/tag_alias.rb
+++ test/models/tag_alias.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class TagAlias < ActiveRecord::Base
-  belongs_to :tag, foreign_key: [:tag_id, :parent_id], required: true
+  unless ENV["SKIP_COMPOSITE_PK"]
+    belongs_to :tag, foreign_key: [:tag_id, :parent_id], required: true
+  end
 end
