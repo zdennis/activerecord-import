24ceeaeecd38745ede39a5b7f1dc4495ecbecb9a
Zack Mariscal <zmariscal@gmail.com>
2024-02-03 08:49:39 -0800

    refactor test trilogy file to extract ENV reference and simplify logic

--- test/adapters/trilogy.rb
+++ test/adapters/trilogy.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
-# ensure we connect to the trilogy adapter
-if ENV['AR_VERSION'].to_f >= 7.1
-  ENV["ARE_DB"] = "trilogy"
-else
+ENV["ARE_DB"] = "trilogy"
+
+if ENV['AR_VERSION'].to_f <= 7.0
   require "activerecord-trilogy-adapter"
   require "trilogy_adapter/connection"
   ActiveRecord::Base.extend TrilogyAdapter::Connection
-  ENV["ARE_DB"] = "trilogy"
 end
